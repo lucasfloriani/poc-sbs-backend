2da7acc7eef0b18d3b0685e064609a8e9556e651
lucasfloriani <lucas.floriane@gmail.com>
2019-04-07 22:34:08 -0300

    Change routes

--- start/routes.js
+++ start/routes.js
@@ -1,14 +1,16 @@
 'use strict'
 
-const Route = use('Route')
 const addPrefixToGroup = (group, prefix) => {
   group.prefix(prefix)
   return group
 }
 
-Route.post('users', 'UserController.store')
-Route.post('gas-stations', 'GasStationController.store')
-Route.post('authenticate', 'AuthController.authenticate')
+const Route = use('Route')
+Route.post('users', 'UserController.store').validator('StoreUser')
+Route.post('gas-stations', 'GasStationController.store').validator(
+  'StoreGasStation'
+)
+Route.post('authenticate', 'AuthController.authenticate').validator('Auth')
 Route.resource('cities', 'CityController').only(['index', 'show'])
 Route.resource('states', 'StateController').only(['index', 'show'])
 Route.resource('fuel-types', 'FuelTypeController').only(['index', 'show'])
@@ -26,19 +28,31 @@ addPrefixToGroup(
     Route.resource('', 'UserController')
       .apiOnly()
       .except('store')
+      .validator(new Map([[['users.update'], ['UpdateUser']]]))
 
     Route.get('complaints', 'ComplaintController.userIndex')
     Route.resource('complaints', 'ComplaintController')
       .apiOnly()
       .except('index')
-    Route.resource('bookmarks', 'BookmarkController').only([
-      'index',
-      'show',
-      'destroy'
-    ])
+      .validator(
+        new Map([
+          [['complaints.store'], ['StoreComplaint']],
+          [['complaints.update'], ['UpdateComplaint']]
+        ])
+      )
+    Route.resource('bookmarks', 'BookmarkController')
+      .only(['index', 'store', 'destroy'])
+      .validator(new Map([[['bookmarks.store'], ['StoreBookmark']]]))
+
     Route.resource('ratings', 'RatingController')
       .apiOnly()
       .except('index')
+      .validator(
+        new Map([
+          [['ratings.store'], ['StoreRating']],
+          [['ratings.update'], ['UpdateRating']]
+        ])
+      )
   }).middleware(['auth', 'onlyUser']),
   'users'
 )
@@ -48,6 +62,16 @@ addPrefixToGroup(
     Route.resource('', 'GasStationController')
       .apiOnly()
       .except('store')
+      .validator(new Map([[['gas-stations.update'], ['UpdateGasStation']]]))
+
+    Route.resource('price-fuel', 'PriceFuelController')
+      .apiOnly()
+      .validator(
+        new Map([
+          [['price-fuel.store'], ['StorePriceFuel']],
+          [['price-fuel.update'], ['UpdatePriceFuel']]
+        ])
+      )
   }).middleware(['auth', 'onlyGasStation']),
   'gas-stations'
 )
