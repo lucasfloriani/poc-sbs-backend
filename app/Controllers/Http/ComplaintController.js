e50488b1c4ab27a7d6bfd1e2107f75d2e7597887
lucasfloriani <lucas.floriane@gmail.com>
2019-08-18 19:43:26 -0300

    Remove unused complaints entrypoints

--- app/Controllers/Http/ComplaintController.js
+++ app/Controllers/Http/ComplaintController.js
@@ -24,31 +24,6 @@ class ComplaintController {
   }
 
   /**
-   * Show a list of all complaints of user.
-   * GET complaints
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async userIndex({ auth }) {
-    const userComplaints = await Complaint.query()
-      .select(
-        'complaints.message',
-        'complaints.gas_station_id',
-        'gas_stations.business_name',
-        'gas_stations.fantasy_name',
-        'gas_stations.cnpj',
-        'gas_stations.anp'
-      )
-      .where('user_id', auth.user.id)
-      .innerJoin('gas_stations', 'complaints.gas_station_id', 'gas_stations.id')
-      .fetch()
-    return userComplaints
-  }
-
-  /**
    * Show a list of all complaints against Gas Station.
    * GET complaints
    *
@@ -90,41 +65,6 @@ class ComplaintController {
   async store({ auth, request }) {
     const data = request.only(['gas_station_id', 'message'])
     const complaint = await Complaint.create({ ...data, user_id: auth.user.id })
-    return complaint
-  }
-
-  /**
-   * Update complaint details.
-   * PUT or PATCH complaints/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update({ auth, params, request, response }) {
-    const complaint = await Complaint.findOrFail(params.id)
-    if (auth.user.id != complaint.user_id) {
-      return response.status(401)
-    }
-    complaint.merge(request.only(['message']))
-    await complaint.save()
-    return complaint
-  }
-
-  /**
-   * Delete a complaint with id.
-   * DELETE complaints/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy({ auth, params, response }) {
-    const complaint = await Complaint.findOrFail(params.id)
-    if (auth.user.id != complaint.user_id) {
-      return response.status(401)
-    }
-    await complaint.delete()
     return complaint
   }
 }
--- start/routes.js
+++ start/routes.js
@@ -19,12 +19,12 @@ Route.get('fuel-types/:id', 'FuelTypeController.show')
 // Public paymentTypes routes
 Route.get('payment-types', 'PaymentTypeController.index')
 Route.get('payment-types/:id', 'PaymentTypeController.show')
+// Public gasStation routes
+Route.get('gas-stations', 'GasStationController.index')
 
 // Private common routes
 addPrefixToGroup(
   Route.group(() => {
-    // Private gasStation routes
-    Route.get('', 'GasStationController.index')
     Route.get(':id', 'GasStationController.show')
     // Private complaints routes
     Route.get(':id/complaints', 'ComplaintController.gasStationIndex')
@@ -42,7 +42,6 @@ addPrefixToGroup(
     Route.post('gas-stations', 'GasStationController.store').validator('StoreGasStation')
     Route.put('gas-stations/:id', 'GasStationController.update').validator('UpdateGasStation')
   }).middleware(['auth', 'onlyAdmin']),
-  // 'gas-stations'
   'admin'
 )
 
@@ -56,12 +55,9 @@ addPrefixToGroup(
     Route.delete(':id', 'UserController.destroy')
 
     // Private user complaints routes
-    Route.get('complaints', 'ComplaintController.userIndex')
     Route.get('complaints/:id', 'ComplaintController.show')
     Route.get('gas-stations/complaint', 'GasStationController.indexComplaint')
     Route.post('complaints', 'ComplaintController.store').validator('StoreComplaint')
-    Route.put('complaints/:id', 'ComplaintController.update').validator('UpdateComplaint')
-    Route.delete('complaints/:id', 'ComplaintController.destroy')
 
     // Private user bookmarks routes
     Route.get('bookmarks', 'BookmarkController.index')
