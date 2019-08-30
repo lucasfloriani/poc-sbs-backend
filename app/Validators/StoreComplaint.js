92e7e874f3d0706a610e399ef1c4fff71d637254
lucasfloriani <lucas.floriane@gmail.com>
2019-08-30 15:13:44 -0300

    Add file validation in store complaint

--- app/Validators/StoreComplaint.js
+++ app/Validators/StoreComplaint.js
@@ -6,17 +6,21 @@ class StoreComplaint {
   get rules() {
     return {
       gas_station_id: 'required|integer',
-      message: 'required|string'
+      message: 'required|string',
+      image: 'file|file_ext:png,jpg,jpeg|file_size:5mb|file_types:image'
     }
   }
 
   get messages() {
     return {
       'gas_station_id.required': 'Posto de Gasolina é obrigatório.',
-      'gas_station_id.integer':
-        'Identificador do posto de gasolina é inválido.',
+      'gas_station_id.integer': 'Identificador do posto de gasolina é inválido.',
       'message.required': 'Mensagem é obrigatória.',
-      'message.string': 'Mensagem precisa ser um texto.'
+      'message.string': 'Mensagem precisa ser um texto.',
+      'image.file': 'Formato da imagem incorreto',
+      'image.file_ext': 'Extensão da imagem é inválido',
+      'image.file_size': 'Tamanho da imagem acima do permitido (5 mb)',
+      'image.file_types': 'Arquivo precisa ser uma imagem',
     }
   }
 
